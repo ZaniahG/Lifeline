026977f0e30036a5ca5d16cf4f10ff37d09e2dd3
Zaniah Glover <gzaniah@gmail.com>
2024-06-20 11:00:12 -0400

    UpdateLifeline1.2

---
+++ btscardpge1.js
@@ -0,0 +1,23 @@
+const selectImage = document.querySelector('.select-image');
+const inputFile = document.querySelector('#file');
+const imgArea = document.querySelector('.img-area');
+
+selectImage.addEventListener('click', function () {
+    inputFile.click();
+})
+
+inputFile.addEventListener('change', function () {
+    const image = this.files[0]
+    console.log(image);
+    const reader = new FileReader();
+    reader.onload = ()=> {
+    const imgUrl = reader.result; 
+    const img = document.createElement('img');
+    img.src = imgUrl;
+    imgArea.appendChild(img);
+    imgArea.classList.add('');
+ 
+    }
+    reader.readAsDataURL(image);
+
+})
